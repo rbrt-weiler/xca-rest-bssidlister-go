ee6a355c07cc583ff9fbd4fc1dd0b8645ef7d53d
Robert WEILER <rbrt-weiler@mailbox.org>
2021-02-28 14:46:38 +0100

    Update DevContainer settings.

--- .devcontainer/install-go-modules.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-go get -u github.com/joho/godotenv
-go get -u gitlab.com/rbrt-weiler/go-module-envordef
-go get -u gitlab.com/rbrt-weiler/go-module-xcarestclient
---
+++ .devcontainer/postCreate.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Usual VS Code suspects
+go get -u -v github.com/mdempsky/gocode
+go get -u -v github.com/stamblerre/gocode
+go get -u -v github.com/rogpeppe/godef
+go get -u -v golang.org/x/tools/cmd/goimports
+go get -u -v golang.org/x/lint/golint
+go get -u -v github.com/ramya-rao-a/go-outline
+go get -u -v github.com/uudashr/gopkgs/v2/cmd/gopkgs
+go get -u -v golang.org/x/tools/gopls
